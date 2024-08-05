aa3d00760672cf8267a6af94cd0c535008300962
Nikki <coinslotgaming@gmail.com>
2024-08-05 15:32:13 -0400

    :boom: Refactoring a bit for AWS, breaks everything atm

--- slack.py
+++ slack.py
@@ -16,7 +16,6 @@ import requests
 import sheets
 
 is_raspberry_pi = not sys.platform.startswith("win32")
-pending_messages = []
 
 # Read the configuration files
 try:
@@ -83,40 +82,6 @@ def get_config(sheets_service, spreadsheet_id: int, device_id: str) -> List[str]
         sys.exit()
 
     return device_info
-
-def post_to_slack(channel_id: str, message: str):
-    """
-    Posts a message to Slack using chat.postMessage
-
-    Args:
-        channel (str): the Slack channel to send the message to
-        message (str): the message to send
-    """
-
-    url = "https://slack.com/api/chat.postMessage"
-    headers = {
-        "Content-Type": "application/json; charset=utf-8",
-        "Authorization": f"Bearer {BOT_OAUTH_TOKEN}"
-    }
-    payload = {
-        "channel": channel_id,
-        "text": message
-    }
-
-    # 10 second timeout
-    response = requests.post(url, headers=headers, json=payload, timeout=10)
-    response_data = response.json()
-
-    if not response_data.get("ok"):
-        raise Exception(f"Error posting message: {response_data}")
-
-    # Extract the message ID (timestamp)
-    message_id = response_data.get("ts")
-    pending_messages.append(message_id)
-
-    print(f"Message posted with ID: {message_id}")
-    
-    return message_id
 
 def get_datetime(update_system_time: bool = False) -> str | None:
     """
@@ -231,85 +196,6 @@ def handle_interaction(do_post: bool = True, press_length: int = 0) -> None:
 
     # send a message to Slack or the console
     handle_lambda(device_config, press_type=press_type, do_post=do_post)
-
-# TODO: Separate this code
-
-def lambda_handler(event: dict, context: object):
-    """
-    AWS Lambda function entry point.
-
-    Args:
-        event (dict): the event data from Slack
-        context (object): the runtime information
-
-    Returns:
-        dict: a response object for the HTTP request
-    """
-    # headers = event["headers"]
-    body = event["body"]
-
-    slack_event = json.loads(body)
-
-    event_type = slack_event["event"]["type"]
-    if event_type == "message":
-        handle_message(slack_event["event"])
-    elif event_type == 'reaction_added':
-        handle_reaction_added(slack_event['event'])
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"status": "success"})
-    }
-
-def handle_message(event: dict) -> bool:
-    """
-    Handles messages for lambda_handler
-
-    Args:
-        event (dict): the event data from Slack
-
-    Returns:
-        resolved (bool): whether the message was marked as resolved, for GUI
-    """
-    message = event.get("text")
-    thread_ts = event.get("thread_ts")
-
-    # going to be used in the GUI to determine if we should display the message
-    resolved = False
-
-    if thread_ts in pending_messages:
-        if ":white_check_mark:" in message or ":+1:" in message:
-            print(f"Message thread {thread_ts} has received a resolving response. Marking as resolved.")
-            pending_messages.remove(thread_ts)
-
-            resolved = True
-
-    return resolved
-
-
-def handle_reaction_added(event: dict) -> bool:
-    """
-    Handles reactions for lambda_handler
-
-    Args:
-        event (dict): the event data from Slack
-
-    Returns:
-        resolved (bool): whether the message was marked as resolved, for GUI
-    """
-    reaction = event.get("reaction")
-    item = event.get("item", {})
-    ts = item.get("ts")
-
-    # going to be used in the GUI to determine if we should display the message
-    resolved = False
-
-    if ts in pending_messages:
-        if reaction in ("white_check_mark", "+1"): # no colons in Slack reaction values
-            print(f"Message {ts} has received a resolving reaction. Marking as resolved.")
-            pending_messages.remove(ts)
-
-    return resolved
 
 if __name__ == "__main__":
     # testing
